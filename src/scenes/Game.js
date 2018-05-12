8fd4c2f6963cc7375f325e9605b99dfc8a58c3d1
gtluszcz <gtluszcz@gmail.com>
2018-05-12 18:16:49 +0200

    chmurki

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,9 +2,10 @@ import Phaser from 'phaser'
 
 export default class Game extends Phaser.Scene {
 
+
     constructor ()
     {
-        super({ key: 'Game' });
+        super({ key: 'Game' })
     }
 
     preload ()
@@ -18,33 +19,67 @@ export default class Game extends Phaser.Scene {
 
     }
 
+    setup(){
+        this.CLOUD_SPAWN_TIME = 500
+        this.CLOUD_SPEED = 5
+    }
+
     create ()
     {
-        //this.face = this.add.image(300, 200, 'plane');
-        var config1 = {
-            key: 'plane',
-            x: 400,
-            y: 300
-        };
-        this.plane = this.make.sprite(config1)
+        this.setup()
 
-
-        this.clouds = []
-        for (let i=1;i<=9;i++){
-            let cl = this.add.sprite(100*i,100*i,'cloud'+i)
-            this.clouds.push(cl)
+        //clouds
+        this.lastTimeSpawnedCloud = 0
+        this.clouds = this.add.group();
+        for (let i=1;i<=27;i++){
+            let y = (i%9)+1
+            console.log(y)
+            let cl = this.clouds.create(100*y,100*y,'cloud'+y)
+            this.clouds.add(cl)
+            this.clouds.killAndHide(cl)
         }
 
+        //plane
+        var configPlane = {
+            key: 'plane',
+            x: 400,
+            y: window.innerHeight / 2
+        };
+        this.plane = this.make.sprite(configPlane)
+
 
     }
-    update(){
-        setTimeout(function () {
+    update(time,delta){
+        this.respawnClouds()
+        this.removeCloudsWhenOffScreen()
 
-        },Math.random()*1000)
-
-
-        this.clouds.forEach(el => el.x-=2)
+        console.log(delta/(1000/60))
+        // Move Clouds
+        this.clouds.getChildren().forEach(el => el.x-=this.CLOUD_SPEED*(delta/(1000/60)))
     }
+
+    respawnClouds(){
+        if (this.time.now - this.lastTimeSpawnedCloud > this.CLOUD_SPAWN_TIME){
+            this.lastTimeSpawnedCloud = this.time.now
+            let x = window.innerWidth
+            let y = (Math.random()*(window.innerHeight+400))-200
+            let cl = this.clouds.getFirstDead(false, x, y)
+            if (cl!==null){
+                cl.x += cl.frame.width / 2
+                cl.active=true
+                cl.setVisible(true)
+            }
+        }
+    }
+
+    removeCloudsWhenOffScreen(){
+        this.clouds.getChildren().forEach(el => {
+            if (el.x + el.frame.width < 0){
+                this.clouds.killAndHide(el)
+            }
+        })
+    }
+
 
 }
 
